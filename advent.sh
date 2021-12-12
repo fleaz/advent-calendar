d3877087f486b1d4c1e30f11a9c31956f4680b4f
fleaz <mail@felixbreidenstein.de>
2021-12-12 18:01:09 +0100

    Add possibility to check previous days

--- advent.sh
+++ advent.sh
@@ -12,11 +12,23 @@ if [ ! -f .shuffled ]; then
 fi
 
 MONTH=$(date "+%m")
+
 if [ "$MONTH" -ne 12 ]; then
     echo "It's not not yet december! ;)"
     exit 1
 fi
 
 DAY=$(date "+%d")
-echo "Behind today's door:"
+
+if [ $# -eq 1 ] && [ -n "$1" ]; then
+    if [ "$1" -le "$MONTH" ]; then
+        DAY=$1
+    else
+        echo "You are not allowed to look into the future. This incident will be reported!"
+        echo "https://xkcd.com/838/"
+        exit 1
+    fi
+fi
+
+echo "Behind door number $DAY:"
 sed "${DAY}q;d" .shuffled
